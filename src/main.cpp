136e874e0e530d84b432bb339f9b2ba086fbff52
David Waldmann <hi-davidw@web.de>
2026-08-26 14:14:48 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <array>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,12 +9,20 @@ int main() {
   std::cerr << std::unitbuf;
 
   std::string input;
+  const std::array<std::string, 3> BUILTINS = {"echo", "exit", "type"};
   while(1) {
     std::cout << "$ ";
     std::getline(std::cin, input);
     if (input.substr(0, 4) == "exit")
       break;
-    if (input.substr(0, 5) == "echo ") {
+    if (input.substr(0, 5) == "type ") {
+      // cmd in BUILTINS-array?
+      if (std::find(BUILTINS.begin(), BUILTINS.end(), input.substr(5)) != BUILTINS.end())
+        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
+      else
+        std::cout << input.substr(5) << ": not found" << std::endl;
+    }
+    else if (input.substr(0, 5) == "echo ") {
       std::cout << input.substr(5) << std::endl;
     }
     else
