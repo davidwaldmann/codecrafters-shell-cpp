1a909fb49be1c3649dd5426af021f2cf5f5a39ca
David Waldmann <hi-davidw@web.de>
2026-08-26 21:12:48 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <sstream>
 #include <filesystem>
 #include <unistd.h>
+#include <vector>
+#include <sys/wait.h>
 namespace fs = std::filesystem;
 
 #ifdef _WIN32
@@ -16,15 +18,10 @@ constexpr char PATH_LIST_SEPARATOR = ':';
 
 const std::array<std::string, 3> BUILTINS = {"echo", "exit", "type"};
 
-
-std::string type_cmd(const std::string& args) {
-  // cmd in BUILTINS-array?
-  if (std::find(BUILTINS.begin(), BUILTINS.end(), args) != BUILTINS.end())
-    return "" + args + " is a shell builtin";
-
+std::string find_exec_in_PATH(const std::string& args) {
   const char* path_env = getenv("PATH");
   if (path_env == nullptr)
-    return args + ": not found";
+    return "";
 
   std::stringstream ss_path(path_env);
   std::string path;
@@ -33,9 +30,19 @@ std::string type_cmd(const std::string& args) {
   while (std::getline(ss_path, path, PATH_LIST_SEPARATOR)) {
     std::string executable = path + '/' + args;
     if (access(executable.c_str(), X_OK) != -1)
-      return args + " is " + executable;
+      return executable;
   }
-  return args + ": not found";
+  return "";
+}
+
+std::string type_cmd(const std::string& args) {
+  // cmd in BUILTINS-array?
+  if (std::find(BUILTINS.begin(), BUILTINS.end(), args) != BUILTINS.end())
+    return "" + args + " is a shell builtin";
+  std::string executable = find_exec_in_PATH(args);
+  return (executable != "") ?
+        args + " is " + executable :
+        args + ": not found";
 }
 
 int main() {
@@ -70,6 +77,38 @@ int main() {
 
     else if (command == "echo") {
       std::cout << args << std::endl;
+    }
+
+    else if (std::string executable = find_exec_in_PATH(command); executable != "") {
+      pid_t pid = fork();
+
+      if (pid == 0) {
+        std::stringstream sstream(args);
+        std::string arg;
+        std::vector<std::string> args_vec;
+
+        args_vec.push_back(command);
+        while (std::getline(sstream, arg, ' ')) {
+          args_vec.push_back(arg);
+        }
+        std::vector<char *> argv;
+        for (auto& s : args_vec) {
+          argv.push_back(s.data());
+        }
+        argv.push_back(nullptr);
+
+        execv(executable.c_str(), argv.data());
+
+        // Fehler in exec
+        perror("exec");
+        _exit(1);
+      }
+      else if (pid > 0) {
+        // Parent process
+        waitpid(pid, nullptr, 0);
+      }
+      else
+        perror("fork");
     }
 
     else
