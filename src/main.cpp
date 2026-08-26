a1ae456339f8e27175ac0cec4fd43783cb4f807e
David Waldmann <hi-davidw@web.de>
2026-08-26 16:10:42 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,40 @@
 #include <string>
 #include <algorithm>
 #include <array>
+#include <cstdlib>
+#include <sstream>
+#include <filesystem>
+#include <unistd.h>
+namespace fs = std::filesystem;
+
+#ifdef _WIN32
+constexpr char PATH_LIST_SEPARATOR = ';';
+#else
+constexpr char PATH_LIST_SEPARATOR = ':';
+#endif
 
 const std::array<std::string, 3> BUILTINS = {"echo", "exit", "type"};
 
 
-std::string type_cmd(std::string args) {
+std::string type_cmd(const std::string& args) {
   // cmd in BUILTINS-array?
   if (std::find(BUILTINS.begin(), BUILTINS.end(), args) != BUILTINS.end())
     return "" + args + " is a shell builtin";
-  else
-    return "" + args + ": not found";
+
+  const char* path_env = getenv("PATH");
+  if (path_env == nullptr)
+    return args + ": not found";
+
+  std::stringstream ss_path(path_env);
+  std::string path;
+
+  // iterate over pathes in PATH-variable
+  while (std::getline(ss_path, path, PATH_LIST_SEPARATOR)) {
+    std::string executable = path + '/' + args;
+    if (access(executable.c_str(), X_OK) != -1)
+      return args + " is " + executable;
+  }
+  return args + ": not found";
 }
 
 int main() {
