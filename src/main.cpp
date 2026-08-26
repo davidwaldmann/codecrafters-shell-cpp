2d9b142f24659073c7249a8b85fe61a90761a363
David Waldmann <hi-davidw@web.de>
2026-08-26 15:07:58 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,53 @@
 #include <algorithm>
 #include <array>
 
+const std::array<std::string, 3> BUILTINS = {"echo", "exit", "type"};
+
+
+std::string type_cmd(std::string args) {
+  // cmd in BUILTINS-array?
+  if (std::find(BUILTINS.begin(), BUILTINS.end(), args) != BUILTINS.end())
+    return "" + args + " is a shell builtin";
+  else
+    return "" + args + ": not found";
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::string input;
-  const std::array<std::string, 3> BUILTINS = {"echo", "exit", "type"};
-  while(1) {
+  while(true) {
     std::cout << "$ ";
-    std::getline(std::cin, input);
-    if (input.substr(0, 4) == "exit")
+
+    std::string input;
+    if (!std::getline(std::cin, input))
       break;
-    if (input.substr(0, 5) == "type ") {
-      // cmd in BUILTINS-array?
-      if (std::find(BUILTINS.begin(), BUILTINS.end(), input.substr(5)) != BUILTINS.end())
-        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
-      else
-        std::cout << input.substr(5) << ": not found" << std::endl;
-    }
-    else if (input.substr(0, 5) == "echo ") {
-      std::cout << input.substr(5) << std::endl;
+
+    std::string command;
+    std::string args;
+    // split command by first space
+    const auto space = input.find(' ');
+    if (space != std::string::npos) {
+      command = input.substr(0, space);
+      args = input.substr(space + 1);
     }
     else
-      std::cout << input << ": command not found" << std::endl;
+      command = input;
+
+    if (command == "exit")
+      break;
+    
+    if (command == "type") {
+      std::cout << type_cmd(args) << '\n';
+    }
+
+    else if (command == "echo") {
+      std::cout << args << std::endl;
+    }
+
+    else
+      std::cout << command << ": command not found" << std::endl;
   }
   
 }
